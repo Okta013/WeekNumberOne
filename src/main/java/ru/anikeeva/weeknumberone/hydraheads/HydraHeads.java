2ca4236bbe9b5cfdb46faab32c39edf42db393c0
Okta013 <sindorayka@yandex.ru>
2024-07-08 22:51:50 +0400

    add context.close to HydraHead

--- src/main/java/ru/anikeeva/weeknumberone/hydraheads/HydraHeads.java
+++ src/main/java/ru/anikeeva/weeknumberone/hydraheads/HydraHeads.java
@@ -7,5 +7,6 @@ public class HydraHeads {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Hydra hydra = context.getBean("hydraBean", Hydra.class);
         System.out.println(hydra);
+        context.close();
     }
 }
